78a163153c341da2356ee4298332e6a82e8264b7
Alejandro Zuazo <alejandrodiego70@yahoo.com>
2019-11-28 21:08:29 -0800

    added assembler

---
+++ Cetys/Arqui/assembly.py
@@ -0,0 +1,31 @@
+import binascii
+f = open("instructions","r")
+data = f.readlines()
+lista = []
+
+dick =	{
+	"ADD": 1,
+	"SUB": 2,
+	"MUL": 3,
+	"DIV": 4,
+
+}
+
+for x in range(len(data)):	
+	lista.append(data[x].split())
+
+print(lista)
+
+k = open("output.hex", "wb")
+for i in range(len(lista)):
+	oper = format(int(dick[lista[i][0]]),'04b')
+	oper2 = format(int(lista[i][1]),'04b')
+	bigoper = hex(int(oper+oper2,2))[2:]
+	print("Lenght " + str(len(bigoper)) + " NUM: " + oper +" " +oper2)
+	bigoper if len(bigoper) % 2 == 0  else "0" + bigoper
+	print("Lenght: " + str(len(bigoper))+ " NUM : " + bigoper)
+	binstr = binascii.unhexlify(bigoper)
+	k.write(binstr)
+
+k.close()
+
