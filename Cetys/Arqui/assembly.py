2fa2f9610c149913becc1806269f0f17eeab8e04
Alejandro Zuazo <alejandrodiego70@yahoo.com>
2019-12-03 23:29:12 -0800

    added asm file

--- Cetys/Arqui/assembly.py
+++ Cetys/Arqui/assembly.py
@@ -1,31 +1,29 @@
 import binascii
-f = open("instructions","r")
+f = open("inst.asm","r")
 data = f.readlines()
 lista = []
 
-dick =	{
+dic = {
 
-	"ADD": 0,
-	"SUB": 1,
-	"MUL": 2,
-	"DIV": 3,
-
+	"ADD" : 0,
+	"SUB" : 1,
+	"MUL" : 2,
+	"DIV" : 3
 }
 
-for x in range(len(data)):	
-	lista.append(data[x].split())
-
-print(lista)
-
-
+lista = [ data[x].split() for x in range(len(data)) ]
 
 k = open("output.hex", "wb")
 for i in range(len(lista)):
-	oper = format(int(dick[lista[i][0]]),'04b')
+	
+	oper = format(int(dic[lista[i][0]]),'04b')
 	oper2 = format(int(lista[i][1]),'04b')
+
 	bigoper = hex(int(oper+oper2,2))[2:]
 	bigoper = bigoper if len(bigoper)%2 == 0 else "0"+bigoper 
-	print("Lenght " + str(len(bigoper)) + " NUM: " + oper +" " +oper)
+	
+	print(f'Data: {lista[i]} Length: {len(bigoper)} NUM: {oper} {oper2} HEX NUM: {bigoper}')
+	
 	binstr = binascii.unhexlify(bigoper)
 	k.write(binstr)
 
---
+++ Cetys/Arqui/inst.asm
@@ -0,0 +1,4 @@
+ADD 14
+SUB 11
+MUL 13
+DIV 12
